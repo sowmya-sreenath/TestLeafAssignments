2d362715195967c5c66c9d4b93877a3b35a1671e
Sowmya Sreenath <62900585+sowmya-sreenath@users.noreply.github.com>
2020-04-22 12:17:11 +0530

    Week1Day3- Buttons

---
+++ DealWithbutons.java
@@ -0,0 +1,27 @@
+package week1day3;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DealWithbutons {
+
+	public static void main(String[] args) {
+		//add system properties webdriver.chrome.driver
+				System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
+				//open the chrome browser
+				//chromeDriver (C) by Se test
+				ChromeDriver driver = new ChromeDriver();
+				
+				//Find login bg color
+				
+				
+				//find the font size of the username
+				
+				//find the width of the login button
+				
+				
+				//find the x position of the username textbox
+				
+				
+	}
+
+}
