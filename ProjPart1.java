cfa75f2c45252985688fbd987fedbbbf8402462f
Sowmya Sreenath <62900585+sowmya-sreenath@users.noreply.github.com>
2020-04-24 21:55:23 +0530

    Update ProjPart1.java

--- ProjPart1.java
+++ ProjPart1.java
@@ -11,62 +11,63 @@ public class ProjPart1 {
 	
 public static void main(String[] args) throws InterruptedException {
 		
-//		Launch the Browser:
+		//Launch the Browser - Chrome
 		System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
-		
+		 
+		//Launch ServiceNow
 		driver.get("https://dev92474.service-now.com/");
 		
+		//Maximize & Wait
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
-//		1. Launch the ServiceNow application:
-//		Switch to Frame
+		//Launch the ServiceNow application & Switch to Frame
 		driver.switchTo().frame("gsft_main");
 		
 		
-//		2. Login with valid credentials username as admin and password as India@123
+		//Login with valid credentials username as admin and password as India@123
 		driver.findElementById("user_name").sendKeys("admin");
 		driver.findElementById("user_password").sendKeys("India@123");
 		
 		driver.findElementById("sysverb_login").click();
 		
 		
-//		3. Enter Change in filter navigator and press enter
+		//Enter Change in filter navigator and press enter
 		driver.findElementByXPath("//input[@id='filter']").sendKeys("Change", Keys.ENTER);
 		Thread.sleep(2000);
 		
 		
-//		4. Click on Create new option Under Change
+		//Click on Create new option Under Change
 		driver.findElementByXPath("(//div[text()='Create New'])[3]").click();
 		driver.switchTo().frame("gsft_main");
 		Thread.sleep(2000);
 		
 		
-//		5. Click on the 1st hyperlink
+		//Click on the 1st hyperlink
 		driver.findElementByPartialLinkText("Changes without predefined").click();
 		
 		
-//		6. Get the CHG No
+		//Get the CHG No
 		WebElement requestNumber = driver.findElementById("change_request.number");
 		String text = requestNumber.getAttribute("value");
 		System.out.println("Incident No Original: " + text);
 		
 		
-//		7. Enter the Mandatory field "Short Description"
+		//Enter the Mandatory field "Short Description"
 		driver.findElementByXPath("//input[@id='change_request.short_description']").sendKeys("Service Now Exercise Part 1");
 		
 		
-//		8. Click on the Submit Button
+		//Click on the Submit Button
 		driver.findElementByXPath("(//button[@id='sysverb_insert'])[1]").click();
 		
 		
-//		9. Enter The Incident Number Newly created in Search Field and Press Enter
+		//Enter The Incident Number Newly created in Search Field and Press Enter
 		driver.findElementByXPath("(//input[@class='form-control'])[1]").sendKeys(text, Keys.ENTER);
 		Thread.sleep(2000);
 		
 		
-//		10. Verify the Incident Number
+		//Verify the Incident Number
 		WebElement requestNumber1 = driver.findElementByXPath("//a[@class='linked formlink']");
 		String text2 = requestNumber1.getText();
 		System.out.println("Retreiving Incident No: " + text2);
@@ -76,8 +77,10 @@ public static void main(String[] args) throws InterruptedException {
 		} else {
 			System.out.println("Incident Number is Not Matching");
 		}
+		
 		// Close the browser
-				driver.close();
+		driver.close();
 	}
+
 
 }
